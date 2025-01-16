27d300ab2a82dbecdacc4dea1d893e0d624aea15
codeLeaf <adio.mojeed@gmail.com>
2025-01-16 23:59:35 +0000

    feat: setup pwa for app

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+import withPWA from "next-pwa";
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
